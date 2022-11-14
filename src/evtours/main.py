11cd33e2cfc1d87636db0f2e7ba6908505522adc
Keith Petersen <krp@keithrpetersen.com>
2022-11-14 11:03:03 -0600

    return distances of stations replace requests with httpx upgrade fastapi

--- src/evtours/main.py
+++ src/evtours/main.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime, date
 
 import cattrs
-import requests
+import httpx
 import ujson
 from fastapi import FastAPI, Query
 
@@ -13,6 +13,7 @@ nearest_api = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json"
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -21,12 +22,16 @@ async def root():
 @app.get("/nearest-ten/")
 async def nearest_ten(latitude: float = Query(ge=-90, le=90),
                       longitude: float = Query(ge=-180, le=180)):
-    r = requests.get(f"{nearest_api}?api_key={API_KEY}&latitude={latitude}&longitude={longitude}&fuel_type=ELEC&limit=10")
+    async with httpx.AsyncClient() as client:
+        r = await client.get(f"{nearest_api}?api_key={API_KEY}&latitude={latitude}&longitude={longitude}&fuel_type=ELEC&limit=10")
+
     data = ujson.loads(r.text)
 
     converter = cattrs.Converter()
     converter.register_structure_hook(date, lambda v, _: datetime.fromisoformat(v))
     converter.register_structure_hook(datetime, lambda v, _: datetime.strptime(v, "%Y-%m-%dT%H:%M:%SZ"))
 
-    station = converter.structure(data["fuel_stations"][0], EvStation)
-    return station
+    stations = converter.structure(data["fuel_stations"], list[EvStation])
+    distances = [s.distance_km for s in stations]
+
+    return distances
