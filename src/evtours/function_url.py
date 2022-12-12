1f05d2aeeb8481b17058216fc7745dc35d1eaffe
Keith Petersen <krp@keithrpetersen.com>
2022-12-12 14:01:40 -0600

    create separate requirements-test.txt black formatting

--- src/evtours/function_url.py
+++ src/evtours/function_url.py
@@ -6,10 +6,6 @@ def handler(event, context):
     version = os.environ["APP_VERSION"]
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "Version ": version
-        })
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"Version ": version}),
     }
--- src/evtours/main.py
+++ src/evtours/main.py
@@ -18,27 +18,36 @@ app = FastAPI()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.get("/nearest-ten")
-async def nearest_ten(latitude: float = Query(ge=-90, le=90),
-                      longitude: float = Query(ge=-180, le=180)):
-    ssm = boto3.client('ssm')
-    api_key = ssm.get_parameter(Name="/api_keys/nrel", WithDecryption=True).get("Parameter").get("Value")
+async def nearest_ten(
+    latitude: float = Query(ge=-90, le=90), longitude: float = Query(ge=-180, le=180)
+):
+    ssm = boto3.client("ssm")
+    api_key = (
+        ssm.get_parameter(Name="/api_keys/nrel", WithDecryption=True)
+        .get("Parameter")
+        .get("Value")
+    )
 
     logger.info("nearest-ten called")
     async with httpx.AsyncClient() as client:
         r = await client.get(
-            f"{nearest_api}?api_key={api_key}&latitude={latitude}&longitude={longitude}&fuel_type=ELEC&limit=10")
+            f"{nearest_api}?api_key={api_key}&latitude={latitude}&longitude={longitude}&fuel_type=ELEC&limit=10"
+        )
     logger.info("nrel endpoint called")
     data = ujson.loads(r.text)
 
     converter = cattrs.Converter()
     converter.register_structure_hook(date, lambda v, _: datetime.fromisoformat(v))
-    converter.register_structure_hook(datetime, lambda v, _: datetime.strptime(v, "%Y-%m-%dT%H:%M:%SZ"))
+    converter.register_structure_hook(
+        datetime, lambda v, _: datetime.strptime(v, "%Y-%m-%dT%H:%M:%SZ")
+    )
 
     stations = converter.structure(data["fuel_stations"], list[EvStation])
     distances = [s.distance_km for s in stations]
